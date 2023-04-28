f2796df35cac7fd3dd9defc85f78390c8bdb3a4d
Judith Esmeralda Renderos Rodriguez <rr22006@esfe.agape.edu.sv>
2023-04-28 10:37:14 -0600

    Botón Modificado

--- queso21/Form1.Designer.cs
+++ queso21/Form1.Designer.cs
@@ -33,11 +33,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(406, 162);
+            this.button1.Location = new System.Drawing.Point(135, 82);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(504, 274);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "BOTÓN";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
--- queso21/Form1.cs
+++ queso21/Form1.cs
@@ -19,8 +19,7 @@ namespace queso21
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hola nub" +
-                "");
+            MessageBox.Show("Hola");
         }
     }
 }
